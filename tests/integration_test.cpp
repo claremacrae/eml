d1cde69c500e39981dce4950a96907f409dae22f
Lesley Lai <lesley@lesleylai.info>
2020-10-12 16:18:03 -0600

    Github actions for generate markdown

--- tests/integration_test.cpp
+++ tests/integration_test.cpp
@@ -34,9 +34,42 @@ void verify_eml(const std::string& source)
   }
 }
 
+void verify_for_documentation(const std::string& source)
+{
+  eml::GarbageCollector gc{};
+
+  eml::CompilerConfig config = {eml::SameScopeShadowing::allow};
+  eml::Compiler compiler{gc, config};
+  eml::VM vm{gc};
+
+  std::stringstream ss;
+  ss << "Input:\n" << source << "\n\n";
+
+  if (!source.empty()) {
+    compiler.compile(source)
+        .map([&vm, &ss](auto tuple) {
+          const auto [bytecode, type] = tuple;
+
+          const auto result = vm.interpret(bytecode);
+          REQUIRE(result);
+          ss << "Executes to:\n" << eml::to_string(type, *result) << '\n';
+        })
+        .map_error([&ss](const auto& errors) {
+          ss << "Error message:\n";
+          std::for_each(std::begin(errors), std::end(errors),
+                        [&ss](auto e) { ss << eml::to_string(e); });
+        });
+    ApprovalTests::Approvals::verify(
+        ss.str(),
+        ApprovalTests::Options{}.fileOptions().withFileExtension(".docs.txt"));
+  }
+}
+
 TEST_CASE("Integration Test")
 {
-  verify_eml("if (5 > 1) {2 + 3} else {4 - 6}");
+  const std::string s = "if (5 > 1) {2 + 3} else {4 - 6}";
+  verify_eml(s);
+  verify_for_documentation(s);
 }
 
 TEST_CASE("Test Error")
