ba6c6bf499951eba8f7d60f9beeeb669b5fcd8c3
Lesley Lai <lesley@lesleylai.info>
2020-10-12 16:02:34 -0600

    Add more integration test

--- tests/integration_test.cpp
+++ tests/integration_test.cpp
@@ -24,16 +24,33 @@ void verify_eml(const std::string& source)
           const auto result = vm.interpret(bytecode);
           REQUIRE(result);
           ss << "Executes to:\n" << eml::to_string(type, *result) << '\n';
-          ApprovalTests::Approvals::verify(ss.str());
         })
-        .map_error([](const auto& errors) {
+        .map_error([&ss](const auto& errors) {
+          ss << "Error message:\n";
           std::for_each(std::begin(errors), std::end(errors),
-                        [](auto e) { std::clog << eml::to_string(e); });
+                        [&ss](auto e) { ss << eml::to_string(e); });
         });
+    ApprovalTests::Approvals::verify(ss.str());
   }
 }
 
 TEST_CASE("Integration Test")
 {
   verify_eml("if (5 > 1) {2 + 3} else {4 - 6}");
+}
+
+TEST_CASE("Test Error")
+{
+  verify_eml("if (5 > 1 {2 + 3} else {4 - 6}");
+}
+
+TEST_CASE("Test with String")
+{
+  const std::string s = R"("Hello, " ++ "world")";
+  verify_eml(s);
+}
+
+TEST_CASE("Test with Arithmetics")
+{
+  verify_eml("1 + 3 / 4 * 5 - 10");
 }
